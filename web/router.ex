feb3f539ee15c0fe3d533a5ab48f78f73158c5db
Prabin Deka <pdeka@thoughtworks.com>
2017-07-19 17:46:38 +1000

    Added update and delete

--- web/router.ex
+++ web/router.ex
@@ -16,9 +16,14 @@ defmodule Discuss.Router do
   scope "/", Discuss do
     pipe_through :browser # Use the default browser stack
 
-    get "/", TopicController, :index
-    get "/topics/new", TopicController, :new
-    post "/topics", TopicController, :create
+    # get "/", TopicController, :index
+    # get "/topics/new", TopicController, :new
+    # post "/topics", TopicController, :create
+    # get "/topics/:id/edit", TopicController, :edit
+    # put "/topics/update/:id", TopicController, :update
+
+    resources "/", TopicController
+
   end
 
   # Other scopes may use custom stacks.
